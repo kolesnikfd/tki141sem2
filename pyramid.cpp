35cfcfc8408592bd94ca80ceaa86ce34cd3cdf58
Колесников Фёдор ТКИ-141 <232354079+kolesnikfd@users.noreply.github.com>
2026-05-11 19:50:08 +0300

    Add files via upload

--- pyramid.cpp
+++ pyramid.cpp
@@ -2,11 +2,24 @@
 #include <cmath>
 #include <iostream>
 
-Pyramid::Pyramid(Point3D p1, Point3D p2, Point3D p3, Point3D top) : p1(p1), p2(p2), p3(p3), top(top) {}
+Pyramid::Pyramid(Point3D p1, Point3D p2, Point3D p3, Point3D top) : p1(p1), p2(p2), p3(p3), top(top) 
+{
+    if (p1.distanceTo(p2) == 0 || p2.distanceTo(p3) == 0) {
+        std::cout << "\nGeometry Error: Base points cannot coincide!" << std::endl;
+        exit(1);
+    }
+
+    double s1 = p1.distanceTo(p2);
+    double s2 = p2.distanceTo(p3);
+    
+    if (std::abs(s1 - s2) > 1e-7) {
+        std::cout << "\nGeometry Error: Base sides are not equal!" << std::endl;
+        exit(1);
+    }
+}
 
 double Pyramid::getHeight() const 
 {
-    
     double centerX = (p1.getX() + p3.getX()) / 2.0;
     double centerY = (p1.getY() + p3.getY()) / 2.0;
     double centerZ = (p1.getZ() + p3.getZ()) / 2.0;
